3a6247c67b97ec5b8319b503e5c3509ba2314b17
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-05 20:23:31 +0530

    [SaiVenkat]-Crate:Created MoodAnalyser to return Happy or Sad

---
+++ MoodAnalyseTest/UnitTest1.cs
@@ -0,0 +1,31 @@
+using Moodanalyzers;
+
+namespace MoodAnalyseTest
+{
+    public class Tests
+    {
+
+        [Test]
+        public void GivenSadMessage_WhenAnalyzingMood_ShouldReturnSad()
+        {
+            //Arrange
+            string message = "I am in sad mood";
+            //Act
+            MoodAnalyser analyser = new MoodAnalyser(message);
+            string actual = analyser.analyzeMood(message);
+            //Assert
+            Assert.AreEqual("Sad", actual);
+        }
+        [Test]
+        public void GivenAnyMessage_WhenAnalyzingMood_ShouldReturnHappy()
+        {
+            //Arrange
+            string message = "I am in any mood";
+            //Act
+            MoodAnalyser analyser = new MoodAnalyser(message);
+            string actual = analyser.analyzeMood(message);
+            //Assert
+            Assert.AreEqual("Happy", actual);
+        }
+    }
+}
---
+++ Moodanalyzers/MoodAnalyser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Moodanalyzers
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public string analyzeMood(string message)
+        {
+            if (this.message.ToLower().Contains("sad"))
+                return "Sad";
+            return "Happy";
+        }
+    }
+}
+           
